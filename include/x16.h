3ca7f03a1ef2c12583c950a67f9a5e4cfac31e23
saturnaliam <luciacdev@gmail.com>
2024-02-11 00:03:51 -0600

    changed some color names

--- include/x16.h
+++ include/x16.h
@@ -2,6 +2,7 @@
 #define X16_H_
 
 #include <stdint.h>
+#include <cbm.h>
 
 typedef uint8_t u8;
 typedef int8_t s8;
@@ -10,21 +11,21 @@ typedef int16_t s16;
 
 #define KEY_SUBROUTINE 0xFFE4
 
-#define COLOR_RED 0x1C
-#define COLOR_GREEN 0x1E
-#define COLOR_BLUE 0x1F
-#define COLOR_ORANGE 0x81
-#define COLOR_BLACK 0x90
-#define COLOR_BROWN 0x95
-#define COLOR_PINK 0x96
-#define COLOR_DARK_GRAY 0x97
-#define COLOR_GRAY 0x98
-#define COLOR_LIGHT_GREEN 0x99
-#define COLOR_LIGHT_BLUE 0x9A
-#define COLOR_LIGHT_GRAY 0x9B
-#define COLOR_PURPLE 0x9C
-#define COLOR_YELLOW 0x9E
-#define COLOR_CYAN 0x9F
+#define X16_COLOR_RED 0x1C
+#define X16_COLOR_GREEN 0x1E
+#define X16_COLOR_BLUE 0x1F
+#define X16_COLOR_ORANGE 0x81
+#define X16_COLOR_BLACK 0x90
+#define X16_COLOR_BROWN 0x95
+#define X16_COLOR_PINK 0x96
+#define X16_COLOR_DARK_GRAY 0x97
+#define X16_COLOR_GRAY 0x98
+#define X16_COLOR_LIGHT_GREEN 0x99
+#define X16_COLOR_LIGHT_BLUE 0x9A
+#define X16_COLOR_LIGHT_GRAY 0x9B
+#define X16_COLOR_PURPLE 0x9C
+#define X16_COLOR_YELLOW 0x9E
+#define X16_COLOR_CYAN 0x9F
 
 // make sure keycode is global!
 #define get_key(keycode) \
--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include "include/x16.h"
 
 int main() {
-  printf("%cred\n%cgreen\n%cblue\n%corange\n%cblack\n%cbrown\n%cpink\n%cdark grey\n%cgrey\n%clight green\n%clight blue\n%clight grey\n%cpurple\n%cyellow\n%ccyan", COLOR_RED, COLOR_GREEN, COLOR_BLUE, COLOR_ORANGE, COLOR_BLACK, COLOR_BROWN, COLOR_PINK, COLOR_DARK_GRAY, COLOR_GRAY, COLOR_LIGHT_GREEN, COLOR_LIGHT_BLUE, COLOR_LIGHT_GRAY, COLOR_PURPLE, COLOR_YELLOW, COLOR_CYAN);
+  printf("%cred\n%cgreen\n%cblue\n%corange\n%cblack\n%cbrown\n%cpink\n%cdark grey\n%cgrey\n%clight green\n%clight blue\n%clight grey\n%cpurple\n%cyellow\n%ccyan", X16_COLOR_RED, X16_COLOR_GREEN, X16_COLOR_BLUE, X16_COLOR_ORANGE, X16_COLOR_BLACK, X16_COLOR_BROWN, X16_COLOR_PINK, X16_COLOR_DARK_GRAY, X16_COLOR_GRAY, X16_COLOR_LIGHT_GREEN, X16_COLOR_LIGHT_BLUE, X16_COLOR_LIGHT_GRAY, X16_COLOR_PURPLE, X16_COLOR_YELLOW, X16_COLOR_CYAN);
 
   return 0;
 }
