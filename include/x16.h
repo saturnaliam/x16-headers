2779aa1a917d31c04d8764be5daddd5c789f7d8d
saturnaliam <luciacdev@gmail.com>
2024-02-10 23:51:10 -0600

    color codes added

--- include/x16.h
+++ include/x16.h
@@ -10,6 +10,22 @@ typedef int16_t s16;
 
 #define KEY_SUBROUTINE 0xFFE4
 
+#define COLOR_RED 0x1C
+#define COLOR_GREEN 0x1E
+#define COLOR_BLUE 0x1F
+#define COLOR_ORANGE 0x81
+#define COLOR_BLACK 0x90
+#define COLOR_BROWN 0x95
+#define COLOR_PINK 0x96
+#define COLOR_DARK_GRAY 0x97
+#define COLOR_GRAY 0x98
+#define COLOR_LIGHT_GREEN 0x99
+#define COLOR_LIGHT_BLUE 0x9A
+#define COLOR_LIGHT_GRAY 0x9B
+#define COLOR_PURPLE 0x9C
+#define COLOR_YELLOW 0x9E
+#define COLOR_CYAN 0x9F
+
 // make sure keycode is global!
 #define get_key(keycode) \
   asm("jsr %w", KEY_SUBROUTINE); \
--- main.c
+++ main.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 #include "include/x16.h"
 
-unsigned char keycode;
 int main() {
-  while (1) {
-    get_key(keycode);
+  printf("%cred\n%cgreen\n%cblue\n%corange\n%cblack\n%cbrown\n%cpink\n%cdark grey\n%cgrey\n%clight green\n%clight blue\n%clight grey\n%cpurple\n%cyellow\n%ccyan", COLOR_RED, COLOR_GREEN, COLOR_BLUE, COLOR_ORANGE, COLOR_BLACK, COLOR_BROWN, COLOR_PINK, COLOR_DARK_GRAY, COLOR_GRAY, COLOR_LIGHT_GREEN, COLOR_LIGHT_BLUE, COLOR_LIGHT_GRAY, COLOR_PURPLE, COLOR_YELLOW, COLOR_CYAN);
 
-    if (keycode) {
-      printf("%u\n", keycode);
-    }
-  }
   return 0;
 }
