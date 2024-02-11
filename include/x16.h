ca78f1ba3ba22f9545212efe35cad34b53f0b6fe
saturnaliam <luciacdev@gmail.com>
2024-02-10 19:53:51 -0600

    typedef'd different int sizes

--- include/x16.h
+++ include/x16.h
@@ -1,6 +1,11 @@
 #ifndef X16_H_
 #define X16_H_
 
-#define PRINT_HI printf("hey");
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef int8_t s8;
+typedef uint16_t u16;
+typedef int16_t s16;
 
 #endif // X16_H_
--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 #include "include/x16.h"
 
 int main() {
-  PRINT_HI;
+  u16 i = 0;
+  while (1) {
+    i += 20;
+    printf("%d\n", i);
+  }
   return 0;
 }
